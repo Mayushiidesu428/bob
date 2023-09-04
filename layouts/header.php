962c7faf6c50eefd8efcf1e16b85d641c76d6c49
Mayushiidesu428 <giahuytqk123@gmail.com>
2023-09-05 01:27:36 +0700

    haaa

--- layouts/header.php
+++ layouts/header.php
@@ -6,6 +6,7 @@
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet">
     <link href="assets/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>bob</title>
   </head>
   <body>
